ba79b5952a2435268e7f2aaab56258f4676325a5
dami <dami4_u@msn.com>
2020-09-17 10:50:40 +0100

    Set up CountriesRepository

---
+++ data/src/main/java/com/aten5/data/repositories/CountriesRepositoryImpl.kt
@@ -0,0 +1,33 @@
+package com.aten5.data.repositories
+
+import com.aten5.data.api.ApiService
+import com.aten5.data.entities.AllCountriesItem
+import com.aten5.domain.entities.CountriesEntity
+import com.aten5.domain.repositories.CountriesRepository
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class CountriesRepositoryImpl(private val service: ApiService) : CountriesRepository {
+    override fun getAllCountries(): Observable<List<CountriesEntity>> {
+        return service.getAllCountries()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .map {
+                mapToCountriesEntity(it)
+            }
+            .toObservable()
+
+    }
+
+    private fun mapToCountriesEntity(list: List<AllCountriesItem>):
+            List<CountriesEntity> {
+        return list.map {
+            CountriesEntity(
+                countryName = it.name,
+                population = it.population,
+                flagUrl = it.flag
+            )
+        }
+    }
+}
